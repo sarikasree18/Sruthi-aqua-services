e74a9ed31c7c4a281407c2ed5a4d9ff6d66a35ec
sarikasree18 <sarikasree18@gmail.com>
2025-08-28 21:24:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+<div class="ro-machine">
+  <div class="pipe"></div>
+  <div class="water-stream" id="waterStream"></div>
+</div>
+
+<div class="glass">
+  <div class="glass-water" id="glassWater"></div>
+</div>
+
+<button onclick="turnOn()">Turn On</button>
+<button onclick="turnOff()">Turn Off</button>
+const products = [
+  {
+    name: "RO Aqua Smart",
+    price: "₹12,999",
+    amc: "AMC: ₹999/year",
+    image: "images/aqua1.jpg"
+  },
+  {
+    name: "Pure Aqua Plus",
+    price: "₹15,999",
+    amc: "AMC: ₹1199/year",
+    image: "images/aqua2.jpg"
+  }
+];
